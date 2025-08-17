623ced114278d0e53f664e42d9e0012f4eee9a3d
Poliana Dias <polianadiias@gmail.com>
2025-08-17 15:31:56 -0300

    Parte 2

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -3,19 +3,25 @@ using ProvaPub.Repository;
 
 namespace ProvaPub.Services
 {
-	public class ProductService
-	{
-		TestDbContext _ctx;
+    public class ProductService
+    {
+        private readonly TestDbContext _ctx;
 
-		public ProductService(TestDbContext ctx)
-		{
-			_ctx = ctx;
-		}
+        public ProductService(TestDbContext ctx)
+        {
+            _ctx = ctx;
+        }
 
-		public ProductList  ListProducts(int page)
-		{
-			return new ProductList() {  HasNext=false, TotalCount =10, Products = _ctx.Products.ToList() };
-		}
+        public ProductList ListProducts(int page, int pageSize = 10)
+        {
+            var totalCount = _ctx.Products.Count();
+            var products = _ctx.Products.OrderBy(p => p.Id)
+                                        .Skip((page - 1) * pageSize)
+                                        .Take(pageSize)
+                                        .ToList();
 
-	}
+            return new ProductList() { HasNext = page * pageSize < totalCount, TotalCount = totalCount, Products = products };
+        }
+
+    }
 }
